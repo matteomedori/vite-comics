b7fb28713e402f4f04e4286d6913f68ccd14280d
matteomedori <matteo.medori.97@gmail.com>
2023-12-07 15:39:35 +0100

    feat: struttura di prova header main footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
-<script></script>
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
 
-<template></template>
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+  <AppFooter />
+</template>
 
 <style></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <h1>Prova</h1>
+</template>
+
+<style lang="scss">
+@use "../styles/partials/variables" as *;
+h1 {
+  color: $primary;
+}
+</style>
