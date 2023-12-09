e7bfc8c33c4e01778cedc672fc34909649b5801f
matteomedori <matteo.medori.97@gmail.com>
2023-12-09 16:26:35 +0100

    feat: lista header dinamica e con hover

--- src/components/AppHeaderList.vue
+++ src/components/AppHeaderList.vue
@@ -1,15 +1,30 @@
 <script>
 export default {
   name: "AppHeaderList",
+  data() {
+    return {
+      headerList: [
+        "Characters",
+        "Comics",
+        "Movies",
+        "Tv",
+        "Games",
+        "Collectibles",
+        "Videos",
+        "Fans",
+        "News",
+        "Shop",
+      ],
+    };
+  },
 };
 </script>
 
 <template>
   <ul>
-    <li>prova li</li>
-    <li>prova li</li>
-    <li>prova li</li>
-    <li>prova li</li>
+    <li v-for="elem in headerList">
+      <a href="#">{{ elem }}</a>
+    </li>
   </ul>
 </template>
 
@@ -19,5 +34,31 @@ ul {
   align-items: center;
   list-style-type: none;
   gap: 30px;
+  height: 100%;
+
+  li {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    border-bottom: 5px solid white;
+  }
+
+  a {
+    text-decoration: none;
+    color: black;
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: -1px;
+    font-size: 12px;
+  }
+
+  li:hover a {
+    color: #0282f9;
+  }
+
+  li:hover {
+    border-color: #0282f9;
+  }
 }
 </style>
